6c78dae6a3e3f0c8339af7852a477ed7ef84464b
Kulakov Oleg <olegkulakov77@gmail.com>
2017-01-15 20:53:33 +0300

    Devise installing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+  
   resources :dashboards
   resources :sys_infos
   root 'dashboards#index'
