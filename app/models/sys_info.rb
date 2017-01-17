c8e9abbb037c2bf0648005e15c3436c02a19d529
Kulakov Oleg <olegkulakov77@gmail.com>
2017-01-17 03:32:39 +0300

    Delete recource SysInfo

--- app/models/sys_info.rb
+++
@@ -1,2 +0,0 @@
-class SysInfo < ApplicationRecord
-end
--- db/migrate/20170114161201_create_sys_infos.rb
+++
@@ -1,8 +0,0 @@
-class CreateSysInfos < ActiveRecord::Migration[5.0]
-  def change
-    create_table :sys_infos do |t|
-    	t.integer :memory_used
-      t.timestamps
-    end
-  end
-end
